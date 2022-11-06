aa780bc73b3e80fd2a76155d2c43684d14957a65
Nischay Sharma <nischay.sharma@edvanta.com>
2022-11-06 16:04:43 +0530

    Added new santa

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,38 @@ document.addEventListener('DOMContentLoaded', () => {
   const snow = getElement('snow');
   const trigger = getElement('trigger');
   const jingle = getElement('jingle');
+  var myTimeline = anime.timeline({
+    duration: 1000,
+  });
+
+  myTimeline.add({
+    targets: '.trigger',
+    opacity: 0
+  }).add({
+    targets: '.banner',
+    opacity: 0
+  })
+    .add({
+      targets: '.forest__top',
+      translateY: '50vh',
+    }).add({
+      targets: '.forest__bottom',
+      height: '30vh',
+    }).add({
+      targets: '.forest__bottom',
+      height: '10vh',
+    })
+    .add({
+      targets: '.forest__top',
+      opacity: 1,
+      translateY: '0'
+    }).add({
+      targets: '.trigger',
+      opacity: 1
+    }).add({
+      targets: '.banner',
+      opacity: 1
+    })
 
   trigger.onclick = () => {
     toggleSnowFall(snow)
@@ -13,9 +45,9 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       jingle.pause();
     }
+    myTimeline.restart();
   }
 
-  var myTimeline = anime.timeline(parameters);
 })
 
 /**
